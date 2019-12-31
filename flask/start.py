82619d9ac285e37b19294c0bd49777ebb0477e86
Khramtsov Valentin <khramtsov.valentin@gmail.com>
2019-12-31 11:47:14 +0300

    Final flask stage 1

--- flask/start.py
+++ flask/start.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -10,11 +10,19 @@ messages = []
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/#quickstart
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html')
 
 
-@app.route('/main')
+@app.route('/main', methods=['GET'])
 def main():
     return render_template('main.html', messages=messages)
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+    messages.append(Message(text, tag))
+    return redirect(url_for('main'))
